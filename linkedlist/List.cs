efb9ffbdba558b00115111582e7603516f530547
loloskc <105159640+loloskc@users.noreply.github.com>
2023-09-21 02:45:39 +0500

    Добавьте файлы проекта.

---
+++ linkedlist/List.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace linkedlist
+{
+    public class Node<T> 
+    {
+        public Node(T value)
+        {
+            Value = value;
+        }
+        public Node() { }
+        public T Value { get; set; }
+        public Node<T>? Next { get; set; }
+    }
+
+    internal class ListNode<T>: IEnumerable<T>
+    {
+        Node<T>? head;
+        Node<T>? tail;
+        int count;
+
+        public void Add(T value)
+        {
+            Node<T> current = new Node<T>(value);
+
+            if(head == null)
+            {
+                head = current;
+            }
+            else
+            {
+                tail!.Next = current;
+            }
+            tail = current;
+            count++;
+        }
+
+        public bool Contains(T value)
+        {
+            Node<T> node = head;
+            while(node != null && node.Value != null)
+            {
+                if (node.Value.Equals(value))
+                {
+                    return true;
+                }
+                node = node.Next;
+            }
+            return false;
+        }
+
+
+       public void AppendFirst(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            node.Next = head;
+            head = node;
+            if (count == 0)
+            {
+                tail = head;
+            }
+            count++;
+        }
+
+        public void Loop(Action<T> action)
+        {
+            Node<T> current = head;
+            while(current != null)
+            {
+                action(current.Value);
+                current = current.Next;
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+
+    }
+
+
+}
---
+++ linkedlist/Program.cs
@@ -0,0 +1,22 @@
+﻿using linkedlist;
+using System;
+
+namespace Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode<int> l1 = new ListNode<int>();
+            l1.Add(1);
+            l1.Add(2);
+            l1.Add(3);
+            l1.Add(4);
+            l1.Add(5);
+
+            Console.WriteLine(l1.Contains(3));
+
+            Console.ReadKey();
+        }
+    }
+}
